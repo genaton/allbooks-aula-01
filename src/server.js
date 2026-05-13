ac57c395aed655097982b69db57a16a8dc7a1856
genaton <genaton@gmail>
2026-05-13 08:00:44 -0300

    altera local

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Boas-vindas a api!")
+  console.log("API disponível através em http://localhost:8000")
 })
